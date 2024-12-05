3c036e25cdbe49850064fa096d55bdc866e348e1
2381059 <2381059@unai.edu>
2024-12-05 09:56:55 +0700

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+package binaryoperator;
+
+public class Assignment {
+    public static void main(String[] args) {
+        // 1. Operator assignment dasar
+        int angka = 10;
+        String nama = "Jeremy Panjaitan";
+
+        // 2. Operator assignment aritmatika
+        int x = 5;
+        x += 3;
+        System.out.println("Niai x setelah += 3 adalah "  + x);
+
+        int y = 5;
+        y += 3; // x = x + 3
+        System.out.println("Nilai x setelah += 3 adalah " + x);
+    }
+}
---
+++ Logika.java
@@ -0,0 +1,8 @@
+package binaryoperator;
+
+public class Logika {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false
+    }
+}
